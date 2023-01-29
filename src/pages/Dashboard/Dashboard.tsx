9b02b88a6948317bde410b8445f935f234f15952
stephenleytro <stephen@leytro.com>
2023-01-29 19:57:41 +0100

    dashboard

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,14 +1,37 @@
 /** @format */
 
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
+import CardList from "../../components/CardList";
 import Layout from "../../components/Layout";
-
+import { listUser } from "../../services/api/apiData";
+import { UserDetails } from "../../types";
+import { Column, TableInstance, TableOptions, useTable } from "react-table";
+import Table from "../../components/Table";
+import { COLUMNS, random_item } from "../../services/UI/table/columns";
 const Dashboard = () => {
+  const [userList, setUserList] = useState<UserDetails[]>([]);
+
+  let userData: UserDetails[] = [];
+  const initUserList = async () => {
+    userData = await listUser({});
+
+    setUserList(
+      userData.map((user) => {
+        return { ...user, accountNumber: random_item(["Inactive", "Active", "Blacklisted", "Pending"]) };
+      })
+    );
+  };
+  useEffect(() => {
+    initUserList();
+  }, []);
+
   return (
-    <Layout text="User">
-      <div>Dashboard</div>
+    <Layout text="Dashboard">
+      <CardList />
+      <div style={{ marginTop: "40px" }}>
+        <Table tableColumns={COLUMNS} userList={userList} />
+      </div>
     </Layout>
   );
 };
-
 export default Dashboard;
